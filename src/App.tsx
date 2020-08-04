89d6d27a67b5f6382e8c2eb4be1ae55a090aa589
LK <i91935058@163.com>
2020-08-04 16:47:42 +0800

    chore: add file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { RecoilRoot } from 'recoil'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { Fabric, Stack, initializeIcons, mergeStyles } from '@fluentui/react'
 
-function App() {
+
+initializeIcons()
+
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RecoilRoot>
+      <Fabric>
+        <Stack horizontal verticalFill>
+          <Router>
+            <div></div>
+          </Router>
+        </Stack>
+      </Fabric>
+    </RecoilRoot>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// TODO: 对此项目尝试使用 snowpack，看是否能提高开发编译速度
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
